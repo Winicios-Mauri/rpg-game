e75f0f8d11167c181003ef3d4819e1611cb9572f
Winicios <winiciosmauri19@hotmail.com>
2022-12-18 20:16:21 -0300

    feat: Finish Project

--- src/components/Character/styles.ts
+++ src/components/Character/styles.ts
@@ -4,11 +4,13 @@ export const Container = styled.div<{
   size: number;
   left: number;
   top: number;
+  sidePos: number;
 }>`
   width: ${props => props.size}px;
   height: ${props => props.size}px;
   position: absolute;
   left: ${props => props.left}px;
   top: ${props => props.top}px;
-  background: red;
+  background-image: url('/assets/char.png');
+  background-position : 0px ${props => props.sidePos}px;
 `;
